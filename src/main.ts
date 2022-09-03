ef9e3432de621e1074fd1b58abd08b6abbc496fc
Kayanski <kowalski.kowalskin@gmail.com>
2022-09-03 18:00:45 +0200

    Corrected fs module

--- src/main.ts
+++ src/main.ts
@@ -2,7 +2,7 @@ import { ValidationPipe } from "@nestjs/common";
 import { NestFactory } from "@nestjs/core";
 import { DocumentBuilder, SwaggerModule } from "@nestjs/swagger";
 import { AppModule } from "./app.module";
-import fs from "fs";
+const fs = require("fs");
 import "dotenv/config"
 
 
