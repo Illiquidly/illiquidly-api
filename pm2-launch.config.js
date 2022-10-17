9d3ff6b5effe9b6bbefbe0688fb69549b1df9985
Kayanski <kowalski.kowalskin@gmail.com>
2022-10-17 10:36:58 +0000

    Added a mainnet automatic feeder

--- pm2-launch.config.js
+++ pm2-launch.config.js
@@ -2,9 +2,16 @@ module.exports = {
   apps : [{
     name   : "illiquidly-api",
     script : "npm run start",
-  },{
-    name   : "regular-trade-updates",
+  },
+{
+    name   : "regular-trade-updates-testnet",
     script : "node dist/chain-listener/trigger_update testnet",
+    cron_restart: '*/30 * * * * *',
+    "autorestart" : false
+  },
+{
+    name   : "regular-trade-updates-mainnet",
+    script : "node dist/chain-listener/trigger_update mainnet",
     cron_restart: '*/30 * * * * *',
     "autorestart" : false
   }
