11ea98ccaf54f6896117c0a69f1276856024439c
Kayanski <kowalski.kowalskin@gmail.com>
2023-04-02 12:06:29 +0200

    Update raffle if there is no randomness to provide

--- src/chain-listener/raffles/provide_randomness.ts
+++ src/chain-listener/raffles/provide_randomness.ts
@@ -11,6 +11,7 @@ import { Address } from "../../utils/blockchain/terra_utils";
 import { signingTerraConfig } from "../../utils/configuration";
 import { ConfigType } from "@nestjs/config";
 import { asyncAction } from "../../utils/js/asyncAction";
+import { RafflesService } from "src/raffles/raffles.service";
 const pMap = require("p-map");
 
 @Injectable()
@@ -20,6 +21,7 @@ export class RandomnessProviderService {
 
   constructor(
     @InjectRepository(Raffle) private rafflesRepository: Repository<Raffle>,
+    private readonly raffleService: RafflesService,
     @Inject(signingTerraConfig.KEY) queueConfig: ConfigType<typeof signingTerraConfig>,
   ) {
     this.signingTerraConfig = queueConfig;
@@ -96,6 +98,14 @@ export class RandomnessProviderService {
         const [error, response] = await asyncAction(handler.post(updateMessages));
         if (error) {
           this.logger.error(error);
+          if(error?.toJSON){
+            this.logger.error(error.toJSON())
+          }
+          // If there is an error, we need to update the raffle
+          return pMap(closedRaffles, (raffle)=>{
+            return this.raffleService.updateRaffleAndParticipants(network, raffle.raffleId)
+
+          })
         }
         this.logger.log(`Posted transaction for raffle randomness : ${response}`);
       }
